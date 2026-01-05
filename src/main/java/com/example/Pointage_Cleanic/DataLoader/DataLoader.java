f00e2711b0fc8f0a951e5dd5104ffb64df114878
Ousmane0897 <dioufousmaneeee@gmail.com>
2026-01-05 09:41:47 +0000

    my commit

--- src/main/java/com/example/Pointage_Cleanic/DataLoader/DataLoader.java
+++ src/main/java/com/example/Pointage_Cleanic/DataLoader/DataLoader.java
@@ -20,9 +20,9 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) {
         // Vérifie si l'utilisateur existe déjà
-        if (loginRepository.findByEmail("ousmane.diouf@cleanicsenegal.com").isEmpty()) {
+        if (loginRepository.findByEmail("diarra.niang@cleanicsenegal.com").isEmpty()) {
             User user = new User();
-            user.setEmail("ousmane.diouf@cleanicsenegal.com");
+            user.setEmail("diarra.niang@cleanicsenegal.com");
             user.setPassword(passwordEncoder.encode("admin2025"));
             user.setRole("SUPERADMIN");
             user.setMustChangePassword(true);
